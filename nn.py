2a44746a51522c224fe26a06788f23d7fb421f19
coleman-jarvin <colemanjarvin13@gmail.com>
2026-05-23 23:49:44 -0500

    fix nn.py import

--- nn.py
+++ nn.py
@@ -16,11 +16,14 @@ class Neuron(Module):
             self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
             self.b = Value(0) 
             self.nonlin = nonlin
+
         def __call__(self, x):
             act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
             return act.relu() if self.nonlin else act
+
         def parameters(self):
             return self.w + [self.b]
+
         def __repr__(self):
             return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
